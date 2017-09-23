df7e142e9b0c329b65239d3bfd9622b20fadbcc3
Andi Mancuso <andi@andimancuso.com>
2017-09-23 17:56:56 -0500

    Adding component

---
+++ src/components/FamilySize.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const sizeOptions = ['relatively small', 'somewhat small', 'medium-sized', 'average in size', 'rather large'];
+
+let familyNumber;
+
+export class FamilySize extends React.Component {
+  render() {
+    let size = sizeOptions[Math.floor(Math.random() * sizeOptions.length)];
+    let familyMultiplier;
+
+    if (size === 'rather large') {
+      familyMultiplier = 12;
+    } else if (size === 'medium-sized' || size === 'average in size') {
+      familyMultiplier = 6;
+    } else {
+      familyMultiplier = 0;
+    };
+
+    let familyNumber = (Math.floor(Math.random() * 5 + 1) * 2) + familyMultiplier;
+
+    return (
+      <span>
+        The family is {size}, with {familyNumber} individuals claiming noble blood and official affiliation with the house, whether or not all of them live in the estate.
+      </span>
+    );
+  };
+};
--- src/components/GetNobleHouse.js
+++ src/components/GetNobleHouse.js
@@ -3,11 +3,12 @@ import { Liege } from './NobleHouseHead';
 import { HouseAlignment } from './HouseAlignment';
 import { HouseInfluence } from './HouseInfluence';
 import { HouseHoldings } from './HouseHoldings';
+import { FamilySize } from './FamilySize';
 
 export class GetNobleHouse extends React.Component {
   render() {
     return (
-      <div><Liege /> <HouseAlignment /> <HouseInfluence /> <HouseHoldings /></div>
+      <div><Liege /> <HouseAlignment /> <HouseInfluence /> <HouseHoldings /> <FamilySize /></div>
     );
   };
 };
